4f80725fca2dd63d478ce8a6f20900a6323a6ae0
Joseph Cunningham <joey.was@gmail.com>
2022-11-19 23:41:27 -0800

    using single domain

--- cf-nginx-letsencrypt/certbot/certbot.sh
+++ cf-nginx-letsencrypt/certbot/certbot.sh
@@ -4,8 +4,8 @@ set -e
 
 trap exit INT TERM
 
-if [ -z "$DOMAINS" ]; then
-  echo "DOMAINS environment variable is not set"
+if [ -z "$DOMAIN" ]; then
+  echo "DOMAIN environment variable is not set"
   exit 1;
 fi
 
@@ -18,37 +18,28 @@ if [ "$CERTBOT_TEST_CERT" != "0" ]; then
   test_cert_arg="--test-cert"
 fi
 
-domains_fixed=$(echo "$DOMAINS" | tr -d \")
-domain_list=($domains_fixed)
-emails_fixed=$(echo "$CERTBOT_EMAILS" | tr -d \")
-emails_list=($emails_fixed)
-for i in "${!domain_list[@]}"; do
-  domain="${domain_list[i]}"
+mkdir -p "/var/www/certbot/$DOMAIN"
 
-  mkdir -p "/var/www/certbot/$domain"
+if [ -d "/etc/letsencrypt/live/$DOMAIN" ]; then
+  echo "Let's Encrypt certificate for $DOMAIN already exists"
+  continue
+fi
 
-  if [ -d "/etc/letsencrypt/live/$domain" ]; then
-    echo "Let's Encrypt certificate for $domain already exists"
-    continue
-  fi
+if [ -z "$EMAIL" ]; then
+  email_arg="--register-unsafely-without-email"
+  echo "Obtaining the certificate for $DOMAIN without email"
+else
+  email_arg="--email $EMAIL"
+  echo "Obtaining the certificate for $DOMAIN with email $EMAIL"
+fi
 
-  email="${emails_list[i]}"
-  if [ -z "$email" ]; then
-    email_arg="--register-unsafely-without-email"
-    echo "Obtaining the certificate for $domain without email"
-  else
-    email_arg="--email $email"
-    echo "Obtaining the certificate for $domain with email $email"
-  fi
-
-  certbot certonly \
-    --webroot \
-    -w "/var/www/certbot/$domain" \
-    -d "$domain" \
-    $test_cert_arg \
-    $email_arg \
-    --rsa-key-size "${CERTBOT_RSA_KEY_SIZE:-4096}" \
-    --agree-tos \
-    --noninteractive \
-    --verbose || true
-done
+certbot certonly \
+  --webroot \
+  -w "/var/www/certbot/$DOMAIN" \
+  -d "$DOMAIN" \
+  $test_cert_arg \
+  $email_arg \
+  --rsa-key-size "${CERTBOT_RSA_KEY_SIZE:-4096}" \
+  --agree-tos \
+  --noninteractive \
+  --verbose || true
